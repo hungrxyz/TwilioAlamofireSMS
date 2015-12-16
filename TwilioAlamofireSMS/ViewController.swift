60f9971cb12a4eecd1d82ce80a31057a1a50c532
Devxhkl <devxhkl@outlook.com>
2015-12-16 13:00:29 +0100

    Successfully sent Twilio SMS with Alamofire

--- TwilioAlamofireSMS/ViewController.swift
+++ TwilioAlamofireSMS/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -17,7 +18,18 @@ class ViewController: UIViewController {
   }
   
   func sendSMS() {
+    let parameters = [
+      "To": "RECEIVER PHONE NUMBER",
+      "From": "YOUR TWILIO NUMBER",
+      "Body": "MESSAGE",
+      "MediaUrl": "URL"]
     
+    Alamofire.request(.POST, "https://TWILIO SID:TWILIO AUTH TOKEN@api.twilio.com/2010-04-01/Accounts/TWILIO SID/Messages", parameters: parameters).response { response in
+      print(response.0)
+      print(response.1)
+      print(response.2)
+      print(response.3)
+    }
   }
 }
 
